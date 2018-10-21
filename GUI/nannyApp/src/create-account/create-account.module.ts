db91c544e6b0b7f469c16e43b925a7cf1aca3186
Matthew <matthew.lee.98@gmail.com>
2018-10-20 23:42:42 -0500

    Beginnings of personal info

--- GUI/nannyApp/src/create-account/create-account.module.ts
+++ GUI/nannyApp/src/create-account/create-account.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateAccountComponent } from './create-account.component';
 import { UserPassComponent } from './user-pass/user-pass.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { UserPassComponent } from './user-pass/user-pass.component';
   ],
   declarations: [
     CreateAccountComponent,
-    UserPassComponent
+    UserPassComponent,
+    PersonalInfoComponent
   ],
   exports: [CreateAccountComponent]
 })
