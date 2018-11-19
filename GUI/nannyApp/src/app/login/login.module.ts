c1009237479c1aad45b0dd35ec369d298c25b6b2
Kyra Segraves <ksegraves@smu.edu>
2018-11-19 13:54:24 -0600

    changed login styles

--- GUI/nannyApp/src/app/login/login.module.ts
+++ GUI/nannyApp/src/app/login/login.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { IconsModule } from '../icons/icons.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    IconsModule
   ],
   declarations: [LoginComponent],
   exports: [
