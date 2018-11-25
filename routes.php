06d4617052ef977d0374656edd1a2e1f88be2c8b
bsaunderss <35714573+bsaunderss@users.noreply.github.com>
2018-11-25 15:49:16 -0600

    Add files via upload

--- routes.php
+++ routes.php
@@ -165,61 +165,7 @@
 
     
     
-      //----------------------------------------------------------------------------
-    //CHILDREN
-    
-    /*$app->post('/children/new', function ($request, $response) {
-               $file = file_get_contents("php://input");
-               $data = json_decode($file, true);
-               foreach ($data["children"] as $key => $value) {
-               $name = $value["name"];
-               $gender = $value["gender"];
-               $age = $value["age"];
-               $likes = $value["likes"];
-               $allergies = $value["allergies"];
-               $specialReqs = $value["specialReqs"];
-               $medications = $value["medications"];
-               $username = $value["username"];
-               $sql = "INSERT INTO child_info (name, gender, age, likes, allergies, specialReqs, medications, username) VALUES ('$name', '$gender', '$age', '$likes', '$allergies', '$specialReqs', '$medications', '$username')";
-               $sth = $this->dbConn->prepare($sql);
-               $sth->execute();
-               }
-               return $this->response->withJson($name);
-               });
-    
-    $app->get('/children', function ($request, $response, $args)
-              {$sth= $this->dbConn->prepare("SELECT * FROM child_info");
-              $sth->execute();
-              $child_info = $sth->fetchAll(); return $this->response->withJson($child_info);
-              });*/
-    
-  
-    
-    
-    
-      //----------------------------------------------------------------------------
-    //NANNY_INFO
-    
-    
-    /*$app->post('/nanny_info/new', function ($request, $response) {
-               $input = $request->getParsedBody();
-              $sql = "INSERT INTO nanny_info (yearsExp, minAge, maxAge, minWage, cpr, pet_friendly, can_drive, can_cook, bio, username) VALUES (:yearsExp, :minAge, :maxAge, :minWage, :cpr, :pet_friendly, :can_drive, :can_cook, :bio, :username)";
-               $sth = $this->dbConn->prepare($sql);
-               $sth->bindParam("yearsExp", $input['yearsExp']);
-               $sth->bindParam("cpr", $input['cpr']);
-               $sth->bindParam("can_cook", $input['can_cook']);
-               $sth->bindParam("can_drive", $input['can_drive']);
-               $sth->bindParam("pet_friendly", $input['pet_friendly']);
-               $sth->bindParam("minAge", $input['minAge']);
-               $sth->bindParam("maxAge", $input['maxAge']);
-               $sth->bindParam("minWage", $input['minWage']);
-               $sth->bindParam("bio", $input['bio']);
-               $sth->bindParam("username", $input['username']);
-               $sth->execute();
-               return $this->response->withJson($input);
-               });
-    
-   */
+   
     $app->get('/nanny_info', function ($request, $response, $args)
               {$sth= $this->dbConn->prepare("SELECT * FROM nanny_info");
               $sth->execute();
@@ -243,28 +189,7 @@
               });
     
     
-    
-    
-    
-    
-     //----------------------------------------------------------------------------
-    //NANNY_REFERENCES
-    
-    /*$app->post('/nanny_references/new', function ($request, $response) {
-               $file = file_get_contents("php://input");
-               $data = json_decode($file, true);
-               foreach ($data["references"] as $key => $value) {
-               $name = $value["name"];
-               $email = $value["email"];
-               $phone_number = $value["phone_number"];
-               $username = $value["username"];
-               $sql = "INSERT INTO nanny_references (name, email, phone_number, username) VALUES ('$name', '$email', '$phone_number', '$username')";
-               print_r($data);
-               $sth = $this->dbConn->prepare($sql);
-               $sth->execute();
-               }
-               return $this->response->withJson($name);
-               });*/
+
     
     $app->get('/nanny_references', function ($request, $response, $args)
               {$sth= $this->dbConn->prepare("SELECT * FROM nanny_references");
@@ -432,7 +357,7 @@
               return $this->response->withJson($jobId);
               });
     
-    //the
+    
     $app->put('/jobs/complete', function ($request, $response, $args) {
               $input = $request->getParsedBody();
               $complete = "1";
@@ -444,25 +369,45 @@
              // return $this->response->withJson($jobId);
               });
 
-    
-//----------------------------------------------------------------------------
+
     
   
     
-    //Display jobs
-    $app->get('/jobs', function ($request, $response, $args)
-              {$sth= $this->dbConn->prepare("SELECT * FROM jobs");
+    //Display jobs by parent username
+    $app->get('/jobs/parent/{username}', function ($request, $response, $args)
+              {
+              $username = $request->getAttribute('username');
+              $sth= $this->dbConn->prepare("SELECT * FROM jobs where parentName = '$username'");
               $sth->execute();
               $jobs = $sth->fetchAll();
               return $this->response->withJson($jobs);
               });
     
-    //Display tasks
-    $app->get('/tasks', function ($request, $response, $args)
-              {$sth= $this->dbConn->prepare("SELECT * FROM tasks");
+    //display jobs by nanny username
+    $app->get('/jobs/nanny/{username}', function ($request, $response, $args)
+              {
+              $username = $request->getAttribute('username');
+              $sth= $this->dbConn->prepare("SELECT * FROM jobs where nannyName = '$username'");
               $sth->execute();
-              $tasks = $sth->fetchAll(); return $this->response->withJson($tasks);
+              $jobs = $sth->fetchAll();
+              return $this->response->withJson($jobs);
               });
+    
+    
+    //display tasks for a job id
+    $app->get('/tasks/{job_id}', function ($request, $response, $args)
+              {
+              $job_id = $request->getAttribute('job_id');
+              $sth= $this->dbConn->prepare("SELECT * FROM tasks where id = '$id'");
+              $sth->execute();
+              $tasks = $sth->fetchAll();
+              return $this->response->withJson($tasks);
+              });
+    
+    //----------------------------------------------------------------------------
+    
+    
+
     
     //display nannys in certain zipcode
     $app->get('/nannys/zip/{zip}', function ($request, $response, $args){
